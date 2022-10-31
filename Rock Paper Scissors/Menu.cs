5b4d4db8eabc78ee3a56443555fc05baf07b773a
callme-devil <pransermi@gmail.com>
2022-10-31 17:39:48 +0330

    feat: Refactored DisplayOptions Method

--- Rock Paper Scissors/Menu.cs
+++ Rock Paper Scissors/Menu.cs
@@ -13,13 +13,26 @@ namespace Rock_Paper_Scissors
             SelectedTabIndex = 0;
         }
 
-        public void DisplayOptions()
+        private void DisplayOptions()
         {
             for (int i = 0; i < Options.Length; i++)
             {
                 string currentOption = Options[i];
+
+                if (i == SelectedTabIndex)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
                 WriteLine($"<< {currentOption} >>");
             }
+            ResetColor();
         }
     }
 }
